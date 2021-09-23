fcc06510124ad11a85cc43c6248579446e8e157a
Tim Flood <tflood9@gmail.com>
2021-09-23 16:59:57 -0400

    initial component structure and styled components

---
+++ app/javascript/components/Home/Home.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState } from "react";
+import Jumbotron from "../Jumbotron";
+
+const Home = () => {
+  const [modules, setModules] = useState({
+    modules: [
+      {
+        id: 1,
+        title: "1. Setting up a new Ruby on Rails App with React.",
+        description: "lorem ipsum",
+        active: false,
+      },
+      {
+        id: 2,
+        title: "2. Adding React to an Existing Rails App.",
+        description: "lorem ipsum",
+        active: false,
+      },
+      {
+        id: 3,
+        title: "3. Building a Hello World App.",
+        description: "lorem ipsum",
+        active: false,
+      },
+      {
+        id: 4,
+        title: "4. Adding React Router Dom to your App",
+        description: "lorem ipsum",
+        active: false,
+      },
+    ],
+  });
+  return (
+    <div>
+      <Jumbotron />
+    </div>
+  );
+};
+
+export default Home;
